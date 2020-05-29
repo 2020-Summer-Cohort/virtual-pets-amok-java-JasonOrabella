771e1749a9f98a4d67db45bbeb31773209007faf
Jason Orabella <jasonOrabella@gmail.com>
2020-05-29 17:51:23 -0400

    yo

--- src/main/java/pets_amok/Dog.java
+++ src/main/java/pets_amok/Dog.java
@@ -1,4 +1,10 @@
 package pets_amok;
 
-public abstract class Dog {
+//walk method increases happiness and lowers risk of soiling cages
+//need waste maybe waste is a function
+public abstract class Dog extends VirtualPet{
+    public Dog(String name, int hunger, int boredom, int happiness, int health) {
+        super(name, hunger, boredom, happiness, health);
+    }
+    w
 }
--- src/main/java/pets_amok/VirtualPet.java
+++ src/main/java/pets_amok/VirtualPet.java
@@ -3,61 +3,32 @@ package pets_amok;
 
 public abstract class VirtualPet {
     String name;
-    boolean isLiving = true;
     int hunger;
-    int water;
     int boredom;
-    int waste;
-    int health=(500-(hunger+water+waste));
-    int happiness = (health*4-(boredom*2));
+    int happiness;
+    int health;
 
 
-    public VirtualPet(String name, boolean isLiving, int hunger, int water, int boredom, int waste, int happiness, int health) {
+
+    public VirtualPet(String name,  int hunger,int boredom,int happiness, int health) {
         this.name = name;
-        this.isLiving = isLiving;
         this.hunger = hunger;
-        this.water = water;
         this.boredom = boredom;
-        this.waste = waste;
-        this.happiness = happiness;
-        this.health = health;
+        this.happiness= happiness;
+        this.health=health;
 
-    }
-
-    public void die() {
-        isLiving = false;
-        System.out.println("This animal has died.");
     }
 
     public String getName() {
         return name;
     }
 
-    public boolean isLiving() {
-        return isLiving;
-    }
-
     public int getHunger() {
         return hunger;
-    }
-
-    public int getWater() {
-        return water;
     }
 
     public int getBoredom() {
         return boredom;
     }
 
-    public int getWaste() {
-        return waste;
-    }
-
-    public int getHappiness() {
-        return happiness;
-    }
-
-    public int getHealth() {
-        return health;
-    }
 }
